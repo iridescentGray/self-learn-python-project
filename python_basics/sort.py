496391687fb133a3f5805ea3c4d2ef68a369df2d
zhoujianyu <zhoujianyu@kaikeba.com>
2023-04-05 13:19:08 +0800

    feat: heap sort

---
+++ python_basics/sort.py
@@ -0,0 +1,18 @@
+import heapq
+
+# 数据集
+list1 = [34, 25, 12, 99, 87, 63, 58, 78, 88, 92]
+list2 = [
+    {'name': 'IBM', 'shares': 100, 'price': 91.1},
+    {'name': 'AAPL', 'shares': 50, 'price': 543.22},
+    {'name': 'FB', 'shares': 200, 'price': 21.09},
+    {'name': 'HPQ', 'shares': 35, 'price': 31.75},
+    {'name': 'YHOO', 'shares': 45, 'price': 16.35},
+    {'name': 'ACME', 'shares': 75, 'price': 115.65}
+]
+
+if __name__ == '__main__':
+    # 堆排序——大顶堆
+    print(heapq.nlargest(3, list1))
+    # 堆排序——小顶堆
+    print(heapq.nsmallest(3, list1))
