856ac3feb4f81b25bfa5a69dae53733939d2301a
zhoujianyu <colorfulgray0@gmail.com>
2023-05-12 13:18:27 +0800

    feat:  learn pytest

---
+++ test/unit_testing/mypytest/parametrize_case.py
@@ -0,0 +1,33 @@
+import pytest
+
+from test_method import string_upper
+
+"""
+
+"""
+
+
+# 这段测试较为复杂，可以被下面的pytest.mark.parametrize 完全替代
+# def test_string_upper():
+#     assert string_upper('foo') == 'FOO'
+#
+# def test_string_empty(): <.>
+#     assert string_upper('') == ''
+#
+# def test_string_mixed_cases():
+#     assert string_upper('foo BAR') == 'FOO BAR'
+
+
+# 表驱动测试
+@pytest.mark.parametrize(
+    # 参数名称
+    "s,expected",
+    # 参数内容
+    [
+        ("foo", "FOO"),
+        ("", ""),
+        ("foo BAR", "FOO BAR"),
+    ],
+)
+def test_string_upper(s, expected):
+    assert string_upper(s) == expected
