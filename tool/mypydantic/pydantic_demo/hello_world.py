55aaa87d598065ba9cf46b7c4ca44970828347b1
zhoujianyu <colorfulgray0@gmail.com>
2023-05-29 17:32:39 +0800

    feat: learn pydantic model

--- tool/mypydantic/pydantic_demo/hello_world.py
+++ tool/mypydantic/pydantic_demo/hello_world.py
@@ -20,16 +20,23 @@ external_data = {
 }
 
 if __name__ == "__main__":
-    logging.info(f"-----------------------------------------base operation------------------------------------------")
-    user = User(**external_data)
-    logging.info(f"user object type {type(user)}")
-    logging.info(f"get user id field {user.id}")
-    logging.info(f"repr {repr(user.signup_ts)}")
-    logging.info(f"get friends field {repr(user.friends)}")
-    logging.info(f"user.dict {repr(user.dict())}")
+    logging.info(f"-----------------------------------------create instance------------------------------------------")
+    create_user_with_default_value = User(id=1, friends=[1, 2])
+    logging.info(f"create_user_with_default_value:  {create_user_with_default_value}")
+    create_user_by_dict = User(**external_data)
+    logging.info(f"create_user_by_dict:  {create_user_by_dict}")
 
-    logging.info(f"------------------------------------------deal json-----------------------------------------------")
-    user_json = user.json()
+    logging.info(f"-----------------------------------------base operation------------------------------------------")
+    logging.info(f"user object type {type(create_user_by_dict)}")
+    logging.info(f"get user id field {create_user_by_dict.id}")
+    logging.info(f"user repr {repr(create_user_by_dict.signup_ts)}")
+    logging.info(f"get friends field {repr(create_user_by_dict.friends)}")
+    logging.info(f"user.dict {repr(create_user_by_dict.dict())}")
+    logging.info(f"dict(user) {dict(create_user_by_dict)}")
+    logging.info(f"repr {repr(create_user_by_dict.signup_ts)}")
+
+    logging.info(f"------------------------------------------handle json-----------------------------------------------")
+    user_json = create_user_by_dict.json()
     logging.info(f"user_json  {user_json}")
     logging.info(f"user_json object type {type(user_json)}")
 
@@ -37,13 +44,13 @@ if __name__ == "__main__":
     user_from_json = User.parse_raw(user_json)
     logging.info(f"user_from_json object type  {type(user_from_json)}")
     logging.info(f"user_from_json   {user_from_json}")
-    # 'is' return False, '==' return True, it means == is overrider by pydantic
-    logging.info(f"user is user_from_json  {user is user_from_json}")
-    logging.info(f"user==user_from_json  {user == user_from_json}")
+
+    # 'is' return False, '==' return True, it means '== ' is overrider by pydantic
+    logging.info(f"user is user_from_json  {create_user_by_dict is user_from_json}")
+    logging.info(f"user==user_from_json  {create_user_by_dict == user_from_json}")
 
     logging.info(f"------------------------------------------error_test-----------------------------------------------")
     try:
         User(signup_ts="broken", friends=[1, 2, "not number"])
     except ValidationError as e:
-        logging.error(f"ValidationError {repr(user.json())}")
         logging.error(e)
