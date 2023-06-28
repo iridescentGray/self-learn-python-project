bcefa9057a308145843b2eac2195f307302c54fb
zhoujianyu <colorfulgray0@gmail.com>
2023-06-28 18:16:52 +0800

    feat: learn data_analysis

--- data_analysis/data_analysis_hello_world/README.md
+++ data_analysis/data_analysis_hello_world/README.md
@@ -22,6 +22,7 @@
     pandas：
         - https://pandas.pydata.org/docs/
     matplotlib：
+        - https://matplotlib.org/
 
 ## project
 
--- data_analysis/data_analysis_hello_world/matplotlib_draw/base_api.py
+++ data_analysis/data_analysis_hello_world/matplotlib_draw/base_api.py
@@ -18,33 +18,41 @@ if __name__ == '__main__':
     logging.info(
         f"----------------------------同坐标系折线图------------------------------------------"
     )
-    # figure()函数的返回值是一个Figure对象，它代表了绘图使用的画布
+    # figure()函数的返回值是一个Figure对象,它代表了绘图使用的画布
     # figsize- 指定画布的尺寸默认 [6.4, 4.8]
     # dpi-     设置绘图的分辨率
     # facecolor- 设置画布的背景色
-    plt.figure(figsize=(8, 4), dpi=120, facecolor='darkgray')
-    # subplot函数用于创建坐标系，该函数会返回Axes对象，也可以通过上面创建的Figure对象的add_subplot方法或add_axes方法来创建坐标系
-    # 前三个参数分别用来指定整个画布分成几行几列以及当前坐标系的索引，这三个参数的默认值都是1
-    plt.subplot(2, 2, 1)
+    line_chart_in_the_same_coordinate_figure = plt.figure(figsize=(8, 4), dpi=120, facecolor='darkgray')
+    logging.info(f"figure return a {type(line_chart_in_the_same_coordinate_figure)}")
+
+    # 使用add_subplot给画布创建一个或多个子图
+    # 三个参数分别用来指定整个画布分成几行几列以及当前坐标系的索引,这三个参数的默认值都是1
+    line_chart_in_the_same_coordinate = line_chart_in_the_same_coordinate_figure.add_subplot(2, 2, 1)
+    # subplot方法返回一个坐标系对象 matplotlib.axes._axes.Axes
+    logging.info(f"subplot return a {type(line_chart_in_the_same_coordinate)}")
+
+    line_chart_in_the_same_coordinate.set_title("同坐标系折线图")
     x = np.linspace(-2 * np.pi, 2 * np.pi, 120)
     y1, y2 = np.sin(x), np.cos(x)
     # linewidth 定制折线的粗细 linestyle 定制折线的样式 marker参数来定制数据点的标记  color来定制折线的颜色
-    plt.plot(x, y1, linewidth=2, marker='*', color='red')
-    plt.plot(x, y2, linewidth=2, marker='*', color='blue')
+    line_chart_in_the_same_coordinate.plot(x, y1, linewidth=2, marker='*', color='red')
+    line_chart_in_the_same_coordinate.plot(x, y2, linewidth=2, marker='*', color='blue')
     # 显示绘图
     plt.show()
 
     logging.info(
         f"----------------------------分离坐标系折线图------------------------------------------"
     )
+    #使用plt 可以代替面向对象操作
+
     # ：使用subplot创建两个坐标系即可
     plt.figure(figsize=(8, 4), dpi=120)
     # 创建坐标系（第1个图）
-    plt.subplot(2, 1, 1)
-    plt.plot(x, y1, linewidth=2, marker='*', color='red')
+    separate_coordinate1 = plt.subplot(2, 1, 1)
+    separate_coordinate1.plot(x, y1, linewidth=2, marker='*', color='red')
     # 创建坐标系（第2个图）
-    plt.subplot(2, 1, 2)
-    plt.plot(x, y2, linewidth=2, marker='^', color='blue')
+    separate_coordinate2 = plt.subplot(2, 1, 2)
+    separate_coordinate2.plot(x, y2, linewidth=2, marker='^', color='blue')
     plt.show()
 
     logging.info(
@@ -56,7 +64,7 @@ if __name__ == '__main__':
     # 用Figure对象的add_axes方法在现有坐标系中嵌套一个新的坐标系
     # 参数代表了新坐标系在原坐标系中的位置
     ax = fig.add_axes((0.595, 0.6, 0.3, 0.25))
-    # 前两个值是左下角的位置，后两个值是坐标系的宽度和高度
+    # 前两个值是左下角的位置,后两个值是坐标系的宽度和高度
     ax.plot(x, y2, marker='^', color='blue')
     ax = fig.add_axes((0.155, 0.2, 0.3, 0.25))
     ax.plot(x, y2, marker='^', color='green')
