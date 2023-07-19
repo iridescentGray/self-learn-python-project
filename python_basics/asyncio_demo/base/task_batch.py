3d916512c4e1217867667521f642c6bf3d450dc4
zhoujianyu <colorfulgray0@gmail.com>
2023-07-19 15:39:54 +0800

    feat: learn asyncio

--- python_basics/asyncio_demo/base/task_batch.py
+++ python_basics/asyncio_demo/base/task_batch.py
@@ -3,6 +3,12 @@ from pprint import pprint
 
 from python_basics.decorator import async_timed
 
+
+async def delay(seconds):
+    await asyncio.sleep(seconds)
+    return f"我睡了 {seconds} 秒"
+
+
 print(f"-------------------------------use_create_task_create_batch_await_task------------------------------------")
 
 
@@ -32,7 +38,7 @@ async def use_gather_create_batch_await_task():
 
 asyncio.run(use_gather_create_batch_await_task())
 
-print(f"----------------------------------gather_task_group-------------------------------------")
+print(f"------------------------------------------gather_task_group---------------------------------------------")
 
 
 @async_timed
@@ -48,4 +54,61 @@ async def gather_task_group():
 
 asyncio.run(gather_task_group())
 
+print(f"----------------------------------use_as_completed_create_task------------------------------------------")
 
+
+@async_timed
+async def use_as_completed_create_task():
+    tasks = [asyncio.create_task(delay(seconds)) for seconds in (3, 1, 2, 4)]
+    as_completed_result = asyncio.as_completed(tasks)
+    print(type(as_completed_result))  # <class 'generator'>
+    for finished_task in as_completed_result:
+        print(type(finished_task))
+        # if you want to handle error,use try except at here
+        print(await finished_task)
+
+
+asyncio.run(use_as_completed_create_task())
+
+print(f"-------------------------------timeout_for_as_completed_task-----------------------------------------")
+
+
+@async_timed
+async def timeout_for_as_completed_task():
+    tasks = [asyncio.create_task(delay(seconds))
+             for seconds in (1, 3, 4)]
+    for finished_task in asyncio.as_completed(tasks, timeout=2):
+        try:
+            print(await finished_task)
+        except asyncio.TimeoutError:
+            # 即使抛出TimeoutError，但未完成的任务不会受到影响，它们仍然在后台执行
+            print("超时啦")
+
+
+asyncio.run(timeout_for_as_completed_task())
+
+print(f"-------------------------------use_wait_batch_wait_task-----------------------------------------")
+
+
+async def use_wait_batch_wait_task():
+    tasks = [asyncio.create_task(delay(seconds)) for seconds in (3, 2, 4)]
+    # 和 gather 一样，默认会等待所有任务都完成
+    done, pending = await asyncio.wait(tasks, timeout=3)
+    print(f"已完成的任务数: {len(done)}")
+    print(f"未完成的任务数: {len(pending)}")
+
+    for done_task in done:
+        print(await done_task)
+
+
+asyncio.run(use_wait_batch_wait_task())
+
+print(f"-------------------------------wait_first_task_done_then-----------------------------------------")
+
+async def wait_first_task_done_then():
+    tasks = [asyncio.create_task(delay(seconds)) for seconds in range(1, 4)]
+    done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+    print(f"已完成的任务数: {len(done)}")
+    print(f"未完成的任务数: {len(pending)}")
+
+asyncio.run(wait_first_task_done_then())
--- python_basics/asyncio_demo/base/task_batch_error_handle.py
+++ python_basics/asyncio_demo/base/task_batch_error_handle.py
@@ -1,11 +1,13 @@
 import asyncio
 from pprint import pprint
 
-print(f"------------------------catch_error_will_not_affect_other_task-------------------------------------------")
+"""
+对于asyncio.gather、wait，如果某个任务出现异常，那么异常会向上抛给 await 所在的位置
+  如果不希望异常抛出，可以指定return_exceptions的参数为 True，出现异常的任务会直接视为已完成
+  如果将抛出的异常捕获，其它任务不会受到影响
+"""
 
-"""
-一个任务出错，那么将异常捕获之后，其它任务不会受到影响
-"""
+print(f"------------------------catch_error_will_not_affect_other_task-------------------------------------------")
 
 
 async def normal_running():
@@ -30,15 +32,43 @@ async def catch_error_will_not_affect_other_task():
 
 asyncio.run(catch_error_will_not_affect_other_task())
 
-print(f"------------------------handle_gather_error-------------------------------------------")
+print(f"----------------------------------handle_gather_error-------------------------------------------")
 
 
 async def handle_gather_error():
     results = await asyncio.gather(normal_running(), raise_error(),
                                    return_exceptions=True)
+    # Collect both results and exception
     for result in results:
         print(result)
         print(type(result))
 
 
 asyncio.run(handle_gather_error())
+
+print(f"----------------------------------handle_wait_error-------------------------------------------")
+
+
+async def error_delay(seconds):
+    await asyncio.sleep(seconds)
+    if seconds >= 2:
+        raise ValueError(f"我出错了 seconds = {seconds}")
+    return f"我睡了 {seconds} 秒"
+
+
+async def handle_wait_error():
+    tasks = [asyncio.create_task(error_delay(seconds)) for seconds in [1, 3, 2]]
+    done, pending = await asyncio.wait(tasks)
+    print(f"已完成的任务数: {len(done)}")
+    print(f"未完成的任务数: {len(pending)}")
+
+    for done_task in done:
+        exc = done_task.exception()
+        # 需要先检测异常是否为空,在用result()获取运行结果
+        if exc:
+            print(f"error is {exc}")
+        else:
+            print(done_task.result())
+
+
+asyncio.run(handle_wait_error())
