376dce4a67bddf48b6f68e8edaccaaab69923dc5
zhoujianyu <colorfulgray0@gmail.com>
2023-08-23 13:21:47 +0800

    feat: learn docker sdk

---
+++ cloud/my_docker/README.md
@@ -0,0 +1,21 @@
+# docker
+
+Docker SDK
+
+## use
+
+### Used by pyenv virtualenv docker
+
+    pyenv virtualenv  3.10.12 docker
+    pyenv activate docker
+    python -m pip install --upgrade pip
+    cd acrobatics/docker
+    pip install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple/
+
+### Related documents
+
+    docker: https://github.com/docker-library/python
+
+## uninstall
+
+    pyenv virtualenv-delete docker
---
+++ cloud/my_docker/container/stop_by_image_name.py
@@ -0,0 +1,18 @@
+import docker
+
+# 创建 Docker 客户端
+client = docker.from_env()
+
+# 定义要停止的镜像名称
+target_image_name = "your_image_name"
+
+# 获取所有运行中的容器
+running_containers = client.containers.list()
+
+# 停止匹配目标镜像名称的容器
+for container in running_containers:
+    container_image_name = container.image.tags[0] if container.image.tags else ""
+    
+    if target_image_name in container_image_name:
+        container.stop()
+        print(f"Stopped container: {container.id} (Image: {container_image_name})")
