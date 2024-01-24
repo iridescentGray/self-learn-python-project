4682162ff497442f4706553a0757dae78cebf93b
zhoujianyu <colorfulgray0@gmail.com>
2024-01-25 00:06:30 +0800

    feat: learn playwright

--- crawler/my_playwright/hello_world_demo/pytest_demo/test_playwright.py
+++ crawler/my_playwright/hello_world_demo/pytest_demo/test_playwright.py
@@ -3,7 +3,6 @@ import re
 import pytest
 from playwright.sync_api import Page, expect
 
-
 def test_has_title(page: Page):
     page.goto("https://playwright.dev/")
     # Expect a title "to contain" a substring.
