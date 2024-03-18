1d71ddc83567396f7469b92e1b6939cf708d330c
colorfulgray0 <colorfulgray0@gmail.com>
2024-03-18 17:53:34 +0800

    feat: learn loguru

---
+++ tool/loguru_demo/logger_market/log_decorator.py
@@ -0,0 +1,32 @@
+import functools
+from loguru import logger
+
+
+def logger_wraps(*, entry=True, exit=True, level="DEBUG"):
+    """Logging entry and exit of functions
+
+
+    Args:
+        entry (bool, optional): _description_. Defaults to True.
+        exit (bool, optional): _description_. Defaults to True.
+        level (str, optional): _description_. Defaults to "DEBUG".
+    """
+
+    def wrapper(func):
+        name = func.__name__
+
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            logger_ = logger.opt(depth=1)
+            if entry:
+                logger_.log(
+                    level, "Entering '{}' (args={}, kwargs={})", name, args, kwargs
+                )
+            result = func(*args, **kwargs)
+            if exit:
+                logger_.log(level, "Exiting '{}' (result={})", name, result)
+            return result
+
+        return wrapped
+
+    return wrapper
--- tool/loguru_demo/logger_market/simple_logger.py
+++ tool/loguru_demo/logger_market/simple_logger.py
@@ -7,4 +7,5 @@ logging_level = "INFO"
 # Delete existing log handlers，because we can't change default logger‘s logger_leve
 logger.remove()
 logger.add(sink=sys.stderr, level=logging_level)
-logger.add("user_data/logs/data_analysis.log", level=logging_level, rotation="12:00")
+logger.add("logs/simple.log", level=logging_level, rotation="12:00")
+logger.add("logs/error.log", backtrace=False, diagnose=True, level="ERROR")
